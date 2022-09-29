6b4a559b7b915f43fc3872d7470c115469f7dc97
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-29 12:31:43 +0530

    digital timer using react
    i have used setState to update the time according to the user's request.(+,-)

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -7,36 +7,63 @@ const startUrl = 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png '
 const resetUrl = 'https://assets.ccbp.in/frontend/react-js/reset-icon-img.png'
 
 class DigitalTimer extends Component {
+  state = {
+    timeValue: 25,
+    // isStarted: false,
+  }
+
+  onClickMinus = () => {
+    this.setState(prevState => ({timeValue: prevState.timeValue - 1}))
+  }
+
+  onClickPlus = () => {
+    this.setState(prevState => ({timeValue: prevState.timeValue + 1}))
+  }
+
   render() {
+    const {timeValue} = this.state
+
     return (
       <div className="app-container">
         <h1 className="main-heading">Digital Timer</h1>
         <div className="timer-container">
           <div className="timer-image">
             <div className="circle-time">
-              <h1 className="time-heading">25:00</h1>
+              <h1 className="time-heading">{timeValue}:00</h1>
             </div>
           </div>
           <div className="display-container">
             <div className="start-reset-container">
               <div className="icon-name">
-                <img src={startUrl} alt="play icon" className="icon" />
+                <button type="button" className="trans-button">
+                  <img src={startUrl} alt="play icon" className="icon" />
+                </button>
                 <p className="para">Start</p>
               </div>
               <div className="icon-name">
-                <img src={resetUrl} alt="reset icon" className="icon" />
+                <button type="button" className="trans-button">
+                  <img src={resetUrl} alt="reset icon" className="icon" />
+                </button>
                 <p className="para">Reset</p>
               </div>
             </div>
             <p className="mini-para">Set Timer limit</p>
             <div className="increment-container">
-              <button type="button" className="plus-button">
+              <button
+                type="button"
+                className="plus-button"
+                onClick={this.onClickMinus}
+              >
                 -
               </button>
               <button type="button" className="button">
-                25
+                {timeValue}
               </button>
-              <button type="button" className="plus-button">
+              <button
+                type="button"
+                className="plus-button"
+                onClick={this.onClickPlus}
+              >
                 +
               </button>
             </div>
