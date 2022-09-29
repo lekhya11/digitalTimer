c00b91cf4a0976d49a357c8923901fd3e890357c
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-09-29 12:08:43 +0530

    Update index.js

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -2,7 +2,7 @@ import {Component} from 'react'
 
 import './index.css'
 
-// const startUrl = 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png '
+const startUrl = 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png '
 // const pausedUrl = ' https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
 const resetUrl = 'https://assets.ccbp.in/frontend/react-js/reset-icon-img.png'
 
@@ -13,11 +13,17 @@ class DigitalTimer extends Component {
         <h1 className="main-heading">Digital Timer</h1>
         <div className="timer-container">
           <div className="timer-image">
-            <div className="circle-time">25:00</div>
+            <div className="circle-time">
+              <h1 className="time-heading">25:00</h1>
+            </div>
           </div>
           <div className="display-container">
             <div className="start-reset-container">
-              <div>
+              <div className="icon-name">
+                <img src={startUrl} alt="play icon" className="icon" />
+                <p className="para">Start</p>
+              </div>
+              <div className="icon-name">
                 <img src={resetUrl} alt="reset icon" className="icon" />
                 <p className="para">Reset</p>
               </div>
